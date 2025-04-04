26f3cc95b5b42093fee4cc31f3f784bae10bbab2
IvanKhomin <homin151210@gmail.com>
2025-04-04 18:24:42 +0300

    implemented printf

--- src/bootloader/stage2/x86.asm
+++ src/bootloader/stage2/x86.asm
@@ -2,6 +2,42 @@ bits 16
 
 section _TEXT class=CODE
 
+global _x86_div64_32
+_x86_div64_32:
+    ; Make new call frame
+    push bp             ; Save old call frame
+    mov bp, sp          ; Initialize new call frame
+
+    push bx
+
+    ; Divide uppper 32 bits
+    mov eax, [bp + 8]   ; eax - dividend
+    mov ecx, [bp + 12]  ; ecx - divisor
+    xor edx, edx
+    div ecx
+
+    ; Store upper 32 bits of quotient
+    mov bx, [bp + 16]
+    mov [bx + 4], eax
+
+    ; Divide lower 32 bits
+    mov eax, [bp + 4]   ; eax - lower 32 bits
+                        ; edx - old remainder
+    div ecx
+
+    ; Store results
+    mov [bx], eax
+    mov bx, [bp + 18]
+    mov [bx], edx
+
+    pop bx
+
+    ; Restore call frame
+    mov sp, bp
+    pop bp
+
+    ret
+
 ;
 ; int 10h ah=0Eh
 ; args: character, page
@@ -29,7 +65,7 @@ _x86_Video_WriteCharTeletype:
     ; Restore bx
     pop bx
 
-    ; Restore call fram
+    ; Restore call frame
     mov sp, bp
     pop bp
 
